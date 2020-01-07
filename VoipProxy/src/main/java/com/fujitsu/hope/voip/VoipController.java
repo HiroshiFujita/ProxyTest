45296001f8233d2f44d858d869aa44c22d5f6522
fujita <fujita@exec.co.jp>
2020-01-07 17:20:56 +0900

    test

--- VoipProxy/src/main/java/com/fujitsu/hope/voip/VoipController.java
+++ VoipProxy/src/main/java/com/fujitsu/hope/voip/VoipController.java
@@ -5,17 +5,18 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.BodyPublisher;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandler;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/voipproxy")  // (2) リクエストURLの指定
@@ -29,20 +30,52 @@ public class VoipController {
 	public String addAction() throws IOException, InterruptedException, URISyntaxException{
 
 
-        HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
+		//HTTP2を使えるHTTPClientを生成
+        HttpClient httpClient = HttpClient.newBuilder()
+        		.version(HttpClient.Version.HTTP_2).build();
 
         System.out.println(httpClient.version());
 
-        BodyPublisher body = BodyPublishers.noBody();
-        HttpRequest request = HttpRequest.newBuilder().uri(new URI("https://api.sandbox.push.apple.com:443")).POST(body).build(); //Create a GET request for the given URI
+        //Sample https://www.mkyong.com/java/java-11-httpclient-examples/
 
-		BodyHandler<String> bodyHandler = HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8);
-		HttpResponse<String> response = HttpClient.newBuilder().build().send(request, bodyHandler);
+        Map<Object, Object> data = new HashMap<>();
+        data.put("data", "test");
+
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper
+              .writerWithDefaultPrettyPrinter()
+              .writeValueAsString(data);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(BodyPublishers.ofString(requestBody))
+                .uri(URI.create("https://api.sandbox.push.apple.com:443"))
+                .setHeader("User-Agent", "Java 11 HttpClient Bot") // add request header
+                .header("authorization", "bearer eyAia2lkIjogIjhZTDNHM1JSWDciIH0.eyAiaXNzIjogIkM4Nk5WOUpYM0QiLCAiaWF0I" +
+                		"jogIjE0NTkxNDM1ODA2NTAiIH0.MEYCIQDzqyahmH1rz1s-LFNkylXEa2lZ_aOCX4daxxTZkVEGzwIhALvkClnx5m5eAT6" +
+                		"Lxw7LZtEQcH6JENhJTMArwLf3sXwi")
+                .header("apns-push-type" , "alert")
+                .header("apns-id"        , "eabeae54-14a8-11e5-b60b-1697f925ec7b")
+                .header("apns-expiration", "0")
+                .header("apns-priority"  , "10")
+                .header("apns-topic"     , "com.example.MyApp")
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+//        BodyPublisher body = BodyPublishers.noBody();
+
+//        HttpRequest request = HttpRequest.newBuilder().uri(new URI("https://api.sandbox.push.apple.com:443")).POST(body).build(); //Create a GET request for the given URI
+
+		//BodyHandler<String> bodyHandler = HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8);
+		//HttpResponse<String> response = HttpClient.newBuilder().build().send(request, bodyHandler);
 
 		System.out.println(response);
 
 
 	    return "test";
 	}
+
 
 }
